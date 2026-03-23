97e0eef7150557d49726312eb1bd126d1d70f4eb
muhd-sinan-m <lmsinan772@gmail.com>
2026-03-23 17:29:13 +0530

    Added Error Pages

--- app.py
+++ app.py
@@ -283,5 +283,13 @@ def view_papers():
 def about():
     return render_template("about.html")
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def server_error(e):
+    return render_template('500.html'), 500
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 10000)), debug=False)
